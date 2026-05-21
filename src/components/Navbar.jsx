b595a2e158aa3deac06d2bdc29c492dc3613a25e
Joyel15 <joyeldsza@gmail.com>
2026-05-21 10:55:00 +0000

    Add Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import DarkModeToggle from "./DarkModeToggle.jsx";
+
+function Navbar() {
+  return (
+    <nav className="fixed top-0 w-full bg-white/80 dark:bg-black/80 backdrop-blur-sm z-50 border-b border-stone-200 dark:border-gray-800">
+      <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
+        
+        <div className="text-sm md:text-2xl text-emerald-600 font-semibold dark:text-white">
+          <a href="#home">JD</a>
+        </div>
+
+        <div className="contents md:flex md:items-center md:gap-x-8">
+          <div>
+            <ul className="flex space-x-3 md:space-x-8">
+              <li><a href="#about" className="sm:text-xl text-emerald-600 font-semibold dark:text-white hover:text-slate-600 dark:hover:text-gray-400 transition-colors duration-300">About</a></li>
+              <li><a href="#skills" className="sm:text-xl text-emerald-600 font-semibold dark:text-white hover:text-slate-600 dark:hover:text-gray-400 transition-colors duration-300">Skills</a></li>
+              <li><a href="#projects" className="sm:text-xl text-emerald-600 font-semibold dark:text-white hover:text-slate-600 dark:hover:text-gray-400 transition-colors duration-300">Projects</a></li>
+              <li><a href="#contact" className="sm:text-xl text-emerald-600 font-semibold dark:text-white hover:text-slate-600 dark:hover:text-gray-400 transition-colors duration-300">Contact</a></li>
+            </ul>
+          </div>
+
+          <DarkModeToggle />
+        </div>
+
+      </div>
+    </nav>
+  );
+}
+export default Navbar;
